7d871509ac7efada621fc9de3238ddf0a63b3fc7
June-Snow <370443976@qq.com>
2015-05-27 08:55:18 +0800

    add

--- FP-tree/matlab/main.m
+++ FP-tree/matlab/main.m
@@ -1,10 +1,4 @@
 clear all;
 clc;
-load 'testdatas.mat';
-% sup = 1;
-% %datas = zeros(562, 9);
-% datas = {[1 3 4 7],[1 2 3 6 9], [1 2 3 4 5 6 7],[2 3 6 9 ], [1 2 3 4 5 6 7],[1 2 3 4 5 6 8],...
-%     [1 2 3 4 5 6 8 9], [1 2 3 4 5 6 7 8],[1 2 3 4 5 7 8], [1 2 3 6], [1 4 7 9]};
 data = {[1 2 4 5 7], [2 3 4 5 6 7 8], [1 3 4 6 7]};
-%fps = mineFP(data, 3);
-miningFPGrowth(data, 2);
+fps = mineFP(data, 3);
